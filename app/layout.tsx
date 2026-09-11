fb363762e2f23095db04210c07f00cb2315a123e
efremsidiropoulos-ctrl <efrem.sidiropoulos@gmail.com>
2026-09-11 19:59:53 +0200

    feat: add 10-click waiting lobby before site is shown

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
+import { ClickLobby } from "@/components/click-lobby";
 import "./globals.css";
 
 const defaultUrl = process.env.VERCEL_URL
@@ -33,7 +34,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <ClickLobby>{children}</ClickLobby>
         </ThemeProvider>
       </body>
     </html>
